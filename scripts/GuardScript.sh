bbb6337c3317d0094b3b2cc400de52e666fe839e
Lauren-01 <lauren.distels@gmail.com>
2024-04-25 13:16:15 +0200

    export after script

--- scripts/GuardScript.sh
+++ scripts/GuardScript.sh
@@ -3,8 +3,6 @@
 echo "Please wait a moment while CFN-guard is installed"
 curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/aws-cloudformation/cloudformation-guard/main/install-guard.sh | sh
 
-export PATH=~/.guard/bin:$PATH
-
 echo ""
 echo "CFN-Guard is installed!"
 
