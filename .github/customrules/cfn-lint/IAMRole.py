c931a3269dc7c8ea4a0eda6cecb6e6f892285316
Lauren-01 <lauren.distels@gmail.com>
2024-05-06 10:04:28 +0200

    Checks if IAMRole is referenced or not

--- .github/customrules/cfn-lint/IAMRole.py
+++ .github/customrules/cfn-lint/IAMRole.py
@@ -2,26 +2,26 @@
 Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: MIT-0
 """
-
 from cfnlint.rules import CloudFormationLintRule
 from cfnlint.rules import RuleMatch
+from cfnlint.template import Template
 
 
 class IAMRoleRule(CloudFormationLintRule):
-    id = 'W1234' # New Rule ID
-    shortdesc = 'Check if IAM Role is not referenced in the template' # A short description about the rule
-    description = 'This custom rule will check if an iAM Role is defined but not referenced in the template' # (Longer) description about the rule
+    id = 'W1234'  # New Rule ID
+    shortdesc = 'Check if IAM Role is not referenced in the template'  # A short description about the rule
+    description = 'This custom rule will check if an iAM Role is defined but not referenced in the template'  # (Longer) description about the rule
 
     def match(self, cfn):
+        assert isinstance(cfn, Template), "The object cfn should be a template"
+
         matches = []
 
-        IAM_role = cfn.get_resources(['AWS::IAM::Role'])
+        IAM_roles = cfn.get_resources(['AWS::IAM::Role'])
 
-        all_roles = cfn.get_resource_properties(['Ref'])
+        for role in IAM_roles:
+            if cfn.graph.graph.in_degree(role) == 0:
+                message = 'IAM Role "{}" is defined but not referenced'.format(role)
+                matches.append(RuleMatch(['Resources', role], message))
 
-        for ID, IAM_role in IAM_role.items():
-            if ID not in all_roles:
-                message = 'IAM Role "{}" is defined but not referenced'.format(ID)
-                matches.append(RuleMatch(['Resources', ID], message))
-        
         return matches
